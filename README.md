fbfde5e2697c1a2907389ced0a362a52cd358033
Shaikh <samin@teamamericany.com>
2017-12-03 22:24:20 +0600

    inserting fake data in database db faker

--- README.md
+++ README.md
@@ -7,6 +7,15 @@ php artisan make:model Model/Product -a
 
 php artisan make:model Model/Review -a
 
+php artisan migrate
 
+
+```
+
+
+```javascript
+#Runnig the db seed
+
+php artisan db:seed
 
 ```
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,5 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        factory(App\User::class,5)->create();
+        factory(App\Model\Product::class,50)->create();
+        factory(App\Model\Review::class,300)->create();
     }
 }
